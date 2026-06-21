38cff0cba051f4a3c1a7e0b221793c91d16e054b
salwaasshyfa <salwaasshyfaairuddin@student.telkomuniversity.ac.id>
2026-06-21 19:22:12 +0700

    project product service

--- productService/app/Models/Product.php
+++ productService/app/Models/Product.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'price', 'stock'];
-    protected $hidden = ['created_at', 'updated_at'];
+    protected $fillable = ['name', 'price'];
+    protected $hidden = ['price', 'created_at', 'updated_at'];
 }
